167e299a08380fb8b5b8ea6909c7cd132b296e68
mh-saeed <saeedkhan111244@gmail.com>
2021-04-16 11:22:17 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <div>
 
-          <h1>As-Salaam-Alaikum to all the Muslims around the world !</h1>
+          <h1>As-salamu alaykum to all the Muslims around the world !</h1>
         </div>
         <p><strong>Biggest & Happiest News:</strong> There Will be two Ramdans in the year of 2030 😍💕</p>
       </header>
